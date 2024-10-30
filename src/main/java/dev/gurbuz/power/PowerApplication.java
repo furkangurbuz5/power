1dd13ffe6c043b31dcbf530ee99fe14b76a798b1
furkangurbuz <furkangurbuz5@gmail.com>
2024-10-30 15:32:08 +0100

    MVC

--- src/main/java/dev/gurbuz/power/PowerApplication.java
+++ src/main/java/dev/gurbuz/power/PowerApplication.java
@@ -1,13 +1,21 @@
 package dev.gurbuz.power;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PowerApplication {
 
-	public static void main(String[] args) {
+	private static final Logger logger = LoggerFactory.getLogger(PowerApplication.class);
+
+	public static void main(String[] args)
+	{
 		SpringApplication.run(PowerApplication.class, args);
+		logger.info("Automatic build!");
+
+
 	}
 
 }
---
+++ src/main/java/dev/gurbuz/power/WelcomeMessage.java
@@ -0,0 +1,10 @@
+package dev.gurbuz.power;
+
+
+public class WelcomeMessage {
+
+        public String welcomeMessage()
+        {
+                return "Hello, World!";
+        }
+}
